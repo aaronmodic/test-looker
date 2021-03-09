15fb70c4783c6d7ab68a811a63cea8f209d3a030
Aaron Modic <aaron.modic@bytecode.io>
2021-03-09 21:26:20 +0000

    Added a button

--- Test.js
+++ Test.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("Test");
+  var button = document.createElement("button");
+  btn.innerHTML = "Click";
+  document.body.appendChild(btn);
 })
